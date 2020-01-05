e17d6f53baf98000e5eb616cf2dd79173f74ee7e
Philipp Lühmann <spam@philippluehmann.de>
2020-01-05 23:05:59 +0100

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 const App: React.FC = () => {
+  const [message, setMessage] = useState("DEFAULT MESSAGE");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Message: {message}</p>
+      <button onClick={() => {
+        fetch("/.netlify/functions/hello")
+          .then(result => result.json())
+          .then(obj => setMessage(obj.msg));
+      }}>click me</button>
     </div>
   );
 }
