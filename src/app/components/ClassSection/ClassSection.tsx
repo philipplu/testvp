8687e7283bea8887374c1c444b18149891c100d7
Philipp Lühmann <spam@philippluehmann.de>
2020-01-16 16:39:06 +0100

    created space for notch etc.

--- src/app/components/ClassSection/ClassSection.tsx
+++ src/app/components/ClassSection/ClassSection.tsx
@@ -1,16 +1,12 @@
-import { bemModule } from '@jahed/bem'
 import React, { FunctionComponent, useContext } from 'react'
 import { SchoolClass } from '../../../model/SchoolClass'
-import cssModule from './ClassSection.module.scss'
+import { AppContext } from '../../context/AppContext'
 import { Entry } from '../Entry/Entry'
 import { ListGroup, ListGroupTitle } from '../List/List'
-import { AppContext } from '../../context/AppContext'
 
 interface Props {
 	schoolClass: SchoolClass
 }
-
-const bem = bemModule(cssModule)
 
 const ClassSection: FunctionComponent<Props> = ({ schoolClass }) => {
 	const { activeClass } = useContext(AppContext)
